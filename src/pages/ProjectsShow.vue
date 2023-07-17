6e684a49c1a8f5f0a0608c18235b9afeb598a25a
AlessioAbbati <alex_117@live.it>
2023-07-17 19:01:09 +0200

    print on show

--- src/pages/ProjectsShow.vue
+++ src/pages/ProjectsShow.vue
@@ -6,19 +6,23 @@ export default {
     data() {
         return {
             store,
+            project: {},
         }
     },
     created() {
         // fai la richiesta axios
         axios.get(this.store.baseUrl + 'api/projects/' + this.$route.params.slug)
-        .then(response => response.data.data)
+        .then(response => this.project = response.data)
+        console.log(this.project);
     }
+    
 
 };
 </script>
 
 <template>
   <h2>show</h2>
+  <h5>{{ project.title }}</h5>
 </template>
 
 <style>
