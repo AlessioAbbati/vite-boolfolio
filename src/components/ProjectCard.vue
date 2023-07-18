ff3b72425a10e110847838f5384729b210a6ea3c
AlessioAbbati <alex_117@live.it>
2023-07-18 10:24:58 +0200

    add function get imgurl

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -30,13 +30,21 @@ export default {
         //     type: Number,
         //     required: true
         // }
-  }
+    },
+    methods: {
+        getImageUrl(image) {
+			return image
+				? this.store.baseUrl + 'storage/' + image
+				: this.store.baseUrl + 'storage/default.jpg';
+		},
+    }
 };
 </script>
 
 <template>
     <div class="card" style="width: 18rem;">
-        <img :src="this.store.baseUrl + 'storage/' + project.image" class="card-img-top" :alt="project.id">
+        <!-- <img :src="this.store.baseUrl + 'storage/' + project.image" class="card-img-top" :alt="project.id"> -->
+        <img :src="getImageUrl(project.image)" class="card-img-top" :alt="project.id">
         <div class="card-body">
             <h5 class="card-title">{{ project.title }}</h5>
             <p class="card-text">{{ project.description }}</p>
--- src/pages/ProjectsShow.vue
+++ src/pages/ProjectsShow.vue
@@ -14,7 +14,7 @@ export default {
         axios.get(this.store.baseUrl + 'api/projects/' + this.$route.params.slug)
         .then(response => this.project = response.data.results)
         console.log(this.project);
-    }
+    },
     
 
 };
@@ -23,6 +23,7 @@ export default {
 <template>
   <h1>{{ project.title }}</h1>
   <img :src="this.store.baseUrl + 'storage/' + project.image" class="card-img-top" :alt="project.id">
+  <!-- <img :src="this.store.getImageUrl(project.image)" class="card-img-top" :alt="project.id"> -->
   
 </template>
 
